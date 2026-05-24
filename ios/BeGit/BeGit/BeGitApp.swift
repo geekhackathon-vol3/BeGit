84454346b1f77ea85e5bed5c825ef32f7d530d78
Palm <241it002i@s.internet.ac.jp>
2026-05-24 20:51:10 +0900

    Add iOS project

---
+++ ios/BeGit/BeGit/BeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeGitApp.swift
+//  BeGit
+//
+//  Created by palm on 2026/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
