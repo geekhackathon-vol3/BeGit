ab9cada955670f6dc6e760405beeef06d5e25512
Palm <241it002i@s.internet.ac.jp>
2026-09-18 11:22:53 +0900

    タイムライン／リザルトのラベル反映修正

--- ios/BeGit/BeGit/Views/RepositoryActivityCardView.swift
+++ ios/BeGit/BeGit/Views/RepositoryActivityCardView.swift
@@ -511,11 +511,7 @@ private struct BottomLeadingRoundedRectangle: Shape {
 //  activity種別UI定義
 private extension RepositoryActivityType {
     var badgeTitle: String {
-        switch self {
-        case .commit:      "commit"
-        case .pullRequest: "PR"
-        case .memo:        "sorry"
-        }
+        displayName
     }
 
     var badgeIconName: String {
